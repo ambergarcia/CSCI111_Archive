5e4a8751ea4b3b57d2e096b60033e5b0c28ecd55
Amber Garcia <77137171+ambergarcia@users.noreply.github.com>
2021-03-24 21:31:49 -0400

    Update Garcia_Amber_CS111_HW8.cpp

--- Garcia_Amber_CS111_HW8.cpp
+++ Garcia_Amber_CS111_HW8.cpp
@@ -5,33 +5,30 @@ using namespace std;
 int temp = 0;
 long basetemp = 10;
 void subSix(int n){
-// For larger numbers that exceed 1mil, I think this recursive loop
-// May crash.
+
 if(n > 0 && n < 10 && n/basetemp == 0){
     n = n - 1;
-    //cout << n << " FIRST FIRST" << endl;
   }
 
   if(n%basetemp < 10){
     basetemp*=10;
     n = n - pow(10,temp);
-    //cout << n << " First if" << endl;
 
   }
   if((n % basetemp) / (basetemp/10) >= 6){
     temp++;
     basetemp*=10;
     n = n - pow(10,temp);
-    //cout << n << " Second if" << endl;
+
     if(n / (basetemp/10) != 0 ){
-      //cout << n << " Third if" << endl;
+
       subSix(n);
     }else{
       cout << n << endl;
       return;
     }
   }else{
-    //cout << n << " Last if" << endl;
+
     temp++;
     basetemp*=10;
     if(n/(basetemp/10) == 0){
@@ -44,8 +41,6 @@ if(n > 0 && n < 10 && n/basetemp == 0){
 int evens(int n){
 
 }
-
-
 
 int firstTwo(int n){
   if(n < 100){
@@ -63,15 +58,3 @@ int main(){
   cout << temp << endl;
   cout << temp2 << endl;
 }
-/*
-string temp = n + "";
-if(n % 10 >= 6){
-  subSix(n / 10);
-  cout << n << endl;
-}else{
-
-}
-if(n / 10 == 0){
-  return;
-}
-*/
